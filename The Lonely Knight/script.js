73833365615daf22aa29ec395819d1adb08d87ce
Kenan <110567070+mookini@users.noreply.github.com>
2023-02-28 22:09:27 -1000

    Commit to Main

---
+++ The Lonely Knight/script.js
@@ -0,0 +1,17 @@
+//Stat variables
+let health;
+let stamina;
+let goback11;
+let goback;
+let right;
+let left;
+let up;
+let down;
+let moveup;
+let movedown;
+let moveleft;
+let moveright;
+function ranattack(){
+    //Function that starts when their is a random attack
+
+}
